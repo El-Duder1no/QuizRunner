fcab366d0e2fa80c28956f93ad2879e7330ac653
Andrey <udovchenkoae@gmail.com>
2020-04-24 23:58:29 +0700

    Write the start menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <conio.h>
+#include <string>
 
 #include "parseString.h"
 
@@ -6,6 +8,37 @@ using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "RUS");
+
+	char chooseKey;
+	bool registrationState = true;
+
+	cout << "Âûáåðèòå ïóíêò ìåíþ:" << endl;
+	cout << "1 - Âõîä â ó÷åòíóþ çàïèñü" << endl;
+	cout << "2 - Ðåãèñòðàöèÿ" << endl;
+	cout << "ESC - âûõîä" << endl;
 	
+	while (registrationState) {
+		switch (chooseKey = _getch())
+		{
+		case '1' : 
+			cout << "Òû âûáðàë âõîä â ó÷åòêó" << endl;
+			// ÔÓÍÊÖÈß ÂÕÎÄÀ
+			break;
+		case '2':
+			cout << "Òû âûáðàë ðåãèñòðàöèþ" << endl;
+			// ÔÓÍÊÖÈß ÐÅÃÈÑÒÐÀÖÈÈ
+			break;
+		case 27:
+			cout << "Òû âûáðàë âûéòè èç ïðîãðàììû" << endl;
+			registrationState = false;
+			break;
+		default : 
+			cout << "ÎØÈÁÊÀ ÂÂÎÄÀ" << endl;
+			break;
+		}
+	}
+
+	system("PAUSE");
 	return 0;
 }
