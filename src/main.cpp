121339965c750b31870758048ce0a8e767fcf1f6
tryblt <iren09980.icloud.com>
2020-04-25 02:18:13 +0700

    Add three structs

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,38 @@
-#include <iostream>
-
 #include "parseString.h"
-
+#include <Windows.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <vector>
 using namespace std;
+
+struct t1 {
+    string question1;
+    string answer1;
+    string answer2;
+    string answer3;
+    string RightAnswer;
+};
+
+struct t2 {
+    string question2;
+    string answer1;
+    string answer2;
+    string answer3;
+    string answer4;
+    string RightAnswer;
+};
+
+struct t3 {
+    string question3;
+    string RightAnswer1;
+    string RightAnswer2;
+};
+
 
 int main()
 {
-	
-	return 0;
+    system("PAUSE");
+    return 0;
 }
