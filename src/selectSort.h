71020b2152bb7b8fe868903edd14dfbd45848aa7
Andrey <udovchenkoae@gmail.com>
2020-05-04 02:09:44 +0700

    Minor changes in select sort

--- src/selectSort.h
+++ src/selectSort.h
@@ -1,9 +1,10 @@
 #ifndef SELECT_SORT_H
 #define SELECT_SORT_H
 
-#include <vector>
 #include "accountStruct.h"
+#include <utility>
+#include <vector>
 
-void SelectSort(std::vector<Account>* a, int* ind);
+void SelectSort(std::vector<Account>& a, int* ind);
 
-#endif 
+#endif
