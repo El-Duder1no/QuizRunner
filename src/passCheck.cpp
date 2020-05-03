cad46df30aaef4c6c00661ecd954f034f376e00d
Andrey <udovchenkoae@gmail.com>
2020-05-04 02:00:58 +0700

    Minor changes in password check function

--- src/passCheck.cpp
+++ src/passCheck.cpp
@@ -1,21 +1,23 @@
 #include "passCheck.h"
-
 bool isPassCorrect(std::string password)
 {
-	if (password.size() < 8 || password.size() > 25)
-		return 0;
+	if (password.size() < 8 || password.size() > 25) {
+		return false;
+	}
 
 	char* buff = new char[password.length() + 1];
 	strcpy_s(buff, password.length() + 1, password.c_str());
 
 	bool isUpper = false, isDigit = false;
 	for (int i = 0; i < password.length() + 1; i++) {
-		if (isdigit(buff[i])) {
-			isDigit = true;
-		}
-		if (isalpha(buff[i])) {
-			if (isupper(buff[i])) {
-				isUpper = true;
+		if (isgraph(buff[i])) {
+			if (isdigit(buff[i])) {
+				isDigit = true;
+			}
+			if (isalpha(buff[i])) {
+				if (isupper(buff[i])) {
+					isUpper = true;
+				}
 			}
 		}
 	}
