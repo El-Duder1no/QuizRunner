9ff3c69a74a5c6fc02266580a8aac80d3976924d
tryblt <iren09980.icloud.com>
2020-06-01 00:59:57 +0700

    Add testFunctions.h

---
+++ src/testFunctions.h
@@ -0,0 +1,11 @@
+#ifndef TEST_FUNCTIONS
+#define TEST_FUNCTIONS
+#include "Questions.h"
+#include "parseForTXT.h"
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+
+bool CheckPath(std::ifstream& questions, std::vector<Questions>& QuestionsBank);
+#endif
