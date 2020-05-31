32e9a9ae5827f49f28e0a2cf4b10d226ec1b6eaa
tryblt <iren09980.icloud.com>
2020-06-01 00:38:01 +0700

    Add parseForTXT.h

---
+++ src/parseForTXT.h
@@ -0,0 +1,15 @@
+#ifndef PARSE_FOR_TXT
+#define PARSE_FOR_TXT
+#define RIGHT_ANSWER 3
+#define ANSWERS_SIZE 4
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include "Questions.h"
+#include "parseString.h"
+
+void FirstType(std::ifstream &questions, std::vector<Questions>& text);
+void SecondType(std::ifstream &questions, std::vector<Questions>& text);
+void ThirdType(std::ifstream &questions, std::vector<Questions>& text);
+#endif
