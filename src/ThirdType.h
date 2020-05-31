cd8d3455953e9dfaa0beb29dad0cc3a563933465
tryblt <iren09980.icloud.com>
2020-06-01 00:30:10 +0700

    Remove ThirdType.cpp and ThirdType.h

--- src/ThirdType.h
+++
@@ -1,9 +0,0 @@
-#ifndef THIRD_TYPE
-#define THIRD_TYPE
-#include <string>
-#include <vector>
-#include "Questions.h"
-#include "parseString.h"
-void ThirdType(std::ifstream &questions, std::vector<Questions>& text);
-
-#endif
