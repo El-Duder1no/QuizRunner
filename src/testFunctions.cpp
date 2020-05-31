eb4b5864477f45249fff3cbfb6d19ab414653af8
tryblt <iren09980.icloud.com>
2020-06-01 03:10:05 +0700

    Update CheckPath in testFunctions.cpp

--- src/testFunctions.cpp
+++ src/testFunctions.cpp
@@ -1,26 +1,44 @@
 #include "testFunctions.h"
 
-bool CheckPath(std::ifstream& questions, std::vector<Questions>& QuestionsBank)
+bool CheckPath(std::ifstream& questions, std::vector<Questions>& QuestionsBank, const std::string questionsPath)
 {
-    if (!questions) {
+	if (!questions) {
         return false;
     } else {
         questions.seekg(std::ios_base::beg);
         while (!questions.eof()) {
             std::string type;
             getline(questions, type, '\n');
+			switch(atoi(type.c_str())){
+            case 1:
+                ParseFirstQuestions(questions, QuestionsBank);
+                break;
+            case 2:
+                ParseSecondQuestions(questions, QuestionsBank);
+            break;
+            case 3:
+                ParseThirdQuestions(questions, QuestionsBank);
+                break;
+        }
+    }
+        return true;
+}
+}
 
-            if (type == "1") {
-                FirstType(questions, QuestionsBank);
-            }
-
-            if (type == "2") {
-                SecondType(questions, QuestionsBank);
-            }
-            if (type == "3") {
-                ThirdType(questions, QuestionsBank);
-            }
+bool CheckPath(std::fstream& FileTxt, std::vector<Questions>& QuestionForTest)
+{
+    if (FileTxt) {
+        return false;
+    } else {
+        FileTxt.seekg(std::ios_base::beg);
+        while (!FileTxt.eof()) {
+            std::string type;
+            getline(FileTxt, type, '\n');
+            if (!(type == ""))
+            std::cout<<" ";
+                //PROVERKA(FileTxt, QuestionForTest, type);
         }
         return true;
     }
 }
+
--- src/testFunctions.h
+++ src/testFunctions.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
+#include <iostream>
+bool CheckPath(std::ifstream& questions, std::vector<Questions>& QuestionsBank, const std::string questionsPath);
 
-bool CheckPath(std::ifstream& questions, std::vector<Questions>& QuestionsBank);
 #endif
