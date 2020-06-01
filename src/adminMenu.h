7f1090f62a17acb573d3768f1c04fc736604b81a
tryblt <iren09980@icloud.com>
2020-06-01 17:55:14 +0700

    Add adminMenu.h

---
+++ src/adminMenu.h
@@ -0,0 +1,35 @@
+#ifndef adminMenu
+
+#define adminMenu
+#include <Windows.h>
+#include <conio.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include "cls.h"
+#include "testFunctions.h"
+#include "pause.h"
+
+#include "Questions.h"
+
+
+bool Administrator(const std::string questionsPath, const std::string TestPath, const std::string NotInTestPath);
+void AdminMenu();
+void CreateTest(
+        std::vector<Questions>& text,
+        std::vector<Questions>& textBuf,
+        std::vector<Questions>& textOut);
+void EditingTest(std::vector<Questions>& textBuf, std::vector<Questions>& textOut);
+void AddQuestions(
+        std::vector<Questions>& text,
+        std::vector<Questions>& textBuf,
+        std::vector<Questions>& textOut);
+void AddQuestions(std::vector<Questions>& textBuf, std::vector<Questions>& textOut);        
+void PrintQuestions(std::vector<Questions>& questions, int i);
+bool Filing(std::fstream& questions, std::vector<Questions>& textBuf, const std::string questionsPath);	
+void RemoveQuestion(
+        std::vector<Questions>& textBuf, std::vector<Questions>& textOut);
+#endif
