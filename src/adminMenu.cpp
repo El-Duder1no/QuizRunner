c213bdea800552d6707ee84be2a7300e4c8fe501
tryblt <iren09980@icloud.com>
2020-06-01 18:07:59 +0700

    Fix code-style in adminMenu.cpp

--- src/adminMenu.cpp
+++ src/adminMenu.cpp
@@ -9,7 +9,10 @@ void AdminMenu()
     std::cout << ">> ";
 }
 
-bool Administrator(const std::string questionsPath, const std::string TestPath, const std::string NotInTestPath)
+bool Administrator(
+        const std::string questionsPath,
+        const std::string TestPath,
+        const std::string NotInTestPath)
 {
     std::ifstream inputText(questionsPath.c_str());
     std::vector<Questions> QuestionsBank, QuestionsForTest, QuestionsOutOfTest;
@@ -25,36 +28,31 @@ bool Administrator(const std::string questionsPath, const std::string TestPath, 
     }
     if (!CheckPath(NotInTest, QuestionsOutOfTest)) {
         return false;
-    }
-	 else {
+    } else {
         while (adminMenuState) {
             AdminMenu();
             switch (chooseKey = _getch()) {
             case '1': {
                 CLS();
                 CreateTest(QuestionsBank, QuestionsForTest, QuestionsOutOfTest);
-                if (!Filing(Test, QuestionsForTest, TestPath)){
-				
+                if (!Filing(Test, QuestionsForTest, TestPath)) {
                     return 0;
-            }
-				if(!Filing(NotInTest, QuestionsOutOfTest, NotInTestPath))
-                {
-				return 0;
-}
+                }
+                if (!Filing(NotInTest, QuestionsOutOfTest, NotInTestPath)) {
+                    return 0;
+                }
                 break;
             }
             case '2': {
                 CLS();
                 EditingTest(QuestionsForTest, QuestionsOutOfTest);
-                               if (!Filing(Test, QuestionsForTest, TestPath)){
-				
+                if (!Filing(Test, QuestionsForTest, TestPath)) {
                     return 0;
-            }
-				if(!Filing(NotInTest, QuestionsOutOfTest, NotInTestPath))
-                {
-				return 0;
-}
-break;
+                }
+                if (!Filing(NotInTest, QuestionsOutOfTest, NotInTestPath)) {
+                    return 0;
+                }
+                break;
             }
             case 27: {
                 adminMenuState = false;
@@ -68,11 +66,10 @@ break;
             }
             }
         }
-    	return true;
-	}
+        return true;
+    }
     Test.close();
     inputText.close();
-
 }
 
 void CreateTest(
@@ -83,15 +80,14 @@ void CreateTest(
     bool Create = true;
     char chooseKey;
     while (Create) {
-    	
         if (textBuf.empty()) {
             AddQuestions(text, textBuf, textOut);
             return;
         } else {
             std::cout << "У вас уже создан тест, вы можете:" << std::endl
-                 << "1. Удалить существующий тест" << std::endl
-                 << "2. Вернуться в меню" << std::endl
-                 << "<< ";
+                      << "1. Удалить существующий тест" << std::endl
+                      << "2. Вернуться в меню" << std::endl
+                      << "<< ";
 
             switch (chooseKey = _getch()) {
             case '1': {
@@ -116,7 +112,8 @@ void CreateTest(
     }
 }
 
-void EditingTest(std::vector<Questions>& textBuf, std::vector<Questions>& textOut)
+void EditingTest(
+        std::vector<Questions>& textBuf, std::vector<Questions>& textOut)
 {
     bool Editing = true;
     int i = 0;
@@ -124,14 +121,14 @@ void EditingTest(std::vector<Questions>& textBuf, std::vector<Questions>& textOu
     while (Editing) {
         if (!textBuf.empty()) {
             std::cout << "1.Удалить некоторые вопросы из теста" << std::endl
-                 << "2.Добавить вопросы в существубщий тест" << std::endl
-                 << "3.Вернуться в меню." << std::endl
-                 << "<< ";
+                      << "2.Добавить вопросы в существубщий тест" << std::endl
+                      << "3.Вернуться в меню." << std::endl
+                      << "<< ";
             switch (chooseKey = _getch()) {
             case '1': {
                 CLS();
                 RemoveQuestion(textBuf, textOut);
-                ;
+
                 break;
             }
             case '2': {
@@ -153,8 +150,8 @@ void EditingTest(std::vector<Questions>& textBuf, std::vector<Questions>& textOu
             }
         } else {
             std::cout << "Нет существующего теста!" << std::endl
-                 << "1. Вернуться в меню" << std::endl
-                 << "<< ";
+                      << "1. Вернуться в меню" << std::endl
+                      << "<< ";
 
             switch (chooseKey = _getch()) {
             case '1': {
@@ -173,15 +170,16 @@ void EditingTest(std::vector<Questions>& textBuf, std::vector<Questions>& textOu
     }
 }
 
-
 void PrintQuestions(std::vector<Questions>& questions, int i)
 {
-    std::cout << "Номер вопроса: " << i + 1 << " | " << questions.size() << std::endl;
+    std::cout << "Номер вопроса: " << i + 1 << " | " << questions.size()
+              << std::endl;
     std::cout << "Вопрос: " << questions[i].question << std::endl;
     for (int j = 0; j < questions[i].RightAnswers.size(); ++j) {
-        std::cout << "Варианты ответов: " << questions[i].answers[j] << std::endl;
-        std::cout << "Правильные ответы: " << questions[i].RightAnswers[j] << std::
-		endl;
+        std::cout << "Варианты ответов: " << questions[i].answers[j]
+                  << std::endl;
+        std::cout << "Правильные ответы: " << questions[i].RightAnswers[j]
+                  << std::endl;
     }
 }
 
@@ -196,10 +194,10 @@ void AddQuestions(
     while (i < text.size()) {
         PrintQuestions(text, i);
         std::cout << std::endl
-             << "1.Добавить вопрос в тест" << std::endl
-             << "2.Следующий вопрос" << std::endl
-             << "3.Вернуться в меню." << std::endl
-             << "<< ";
+                  << "1.Добавить вопрос в тест" << std::endl
+                  << "2.Следующий вопрос" << std::endl
+                  << "3.Вернуться в меню." << std::endl
+                  << "<< ";
 
         switch (chooseKey = _getch()) {
         case '1': {
@@ -235,10 +233,14 @@ void AddQuestions(
     return;
 }
 
-bool Filing(std::fstream& questions, std::vector<Questions>& textBuf, const std::string questionsPath)
+bool Filing(
+        std::fstream& questions,
+        std::vector<Questions>& textBuf,
+        const std::string questionsPath)
 {
-	questions.close();
-    questions.open(questionsPath.c_str(),std::ofstream::out | std::ofstream::trunc);
+    questions.close();
+    questions.open(
+            questionsPath.c_str(), std::ofstream::out | std::ofstream::trunc);
     int i = 0;
     if (!questions) {
         return false;
@@ -256,17 +258,18 @@ bool Filing(std::fstream& questions, std::vector<Questions>& textBuf, const std:
     return true;
 }
 
-void AddQuestions(std::vector<Questions>& textBuf, std::vector<Questions>& textOut)
+void AddQuestions(
+        std::vector<Questions>& textBuf, std::vector<Questions>& textOut)
 {
     int i = 0, chooseKey;
     while (i < textOut.size()) {
         PrintQuestions(textOut, i);
 
         std::cout << std::endl
-             << "1.Добавить вопрос в тест" << std::endl
-             << "2.Следующий вопрос" << std::endl
-             << "3.Вернуться назад" << std::endl
-             << "<< ";
+                  << "1.Добавить вопрос в тест" << std::endl
+                  << "2.Следующий вопрос" << std::endl
+                  << "3.Вернуться назад" << std::endl
+                  << "<< ";
 
         switch (chooseKey = _getch()) {
         case '1': {
@@ -303,10 +306,10 @@ void RemoveQuestion(
     while (i < z) {
         PrintQuestions(textBuf, i);
         std::cout << std::endl
-             << "1.Удалить вопрос из теста" << std::endl
-             << "2.Следующий вопрос" << std::endl
-             << "3.Вернуться в меню." << std::endl
-             << "<< ";
+                  << "1.Удалить вопрос из теста" << std::endl
+                  << "2.Следующий вопрос" << std::endl
+                  << "3.Вернуться в меню." << std::endl
+                  << "<< ";
 
         switch (chooseKey = _getch()) {
         case '1': {
