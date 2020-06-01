bf7e174d042c22c739bbf2f16cfde73ccb43f1e9
tryblt <iren09980.icloud.com>
2020-06-01 15:24:58 +0700

    Fix code-style in parseForTXT.h

--- src/parseForTXT.h
+++ src/parseForTXT.h
@@ -2,17 +2,21 @@
 #define PARSE_FOR_TXT
 #define RIGHT_ANSWER 3
 #define ANSWERS_SIZE 4
+#include "Questions.h"
+#include "parseString.h"
+#include <cstdlib>
 #include <fstream>
 #include <string>
 #include <vector>
-#include <cstdlib>
-#include "Questions.h"
-#include "parseString.h"
 
-void ParseFirstQuestions(std::ifstream& questions, std::vector<Questions>& text);
+void ParseFirstQuestions(
+        std::ifstream& questions, std::vector<Questions>& text);
 void ParseSecondQuestions(
         std::ifstream& questions, std::vector<Questions>& text);
-void ParseThirdQuestions(std::ifstream& questions, std::vector<Questions>& text);
+void ParseThirdQuestions(
+        std::ifstream& questions, std::vector<Questions>& text);
 void ParseTest(
-        std::fstream& questions, std::vector<Questions>& text, std::string type);
+        std::fstream& questions,
+        std::vector<Questions>& text,
+        std::string type);
 #endif
