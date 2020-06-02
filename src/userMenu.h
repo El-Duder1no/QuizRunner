9e3ac6e78cd7d2a10b8b741aea3d1b7fbc8ce3b5
tryblt <iren09980@icloud.com>
2020-06-02 20:46:13 +0700

    Minor changes in userMenu.h

--- src/userMenu.h
+++ src/userMenu.h
@@ -11,10 +11,14 @@
 #include "cls.h"
 #include "pause.h"
 #include "resultStruct.h"
+#include "testFunctions.h"
 #include "parseString.h"
+#include "selectSort.h"
+#include "binSearch.h"
 #include "accountStruct.h"
 #include "currentUserStruct.h"
 
 bool User(const std::string ResultsPath, currentUser& user);
 void UserMenu();
+bool CheckResult(currentUser& user, std::vector<Results>& usersResults);
 #endif
