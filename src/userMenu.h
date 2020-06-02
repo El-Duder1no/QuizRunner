473cf05f3927e43bfb14836de0358df7dc9ce28f
tryblt <iren09980@icloud.com>
2020-06-02 16:37:14 +0700

    Add functions to userMenu.h

---
+++ src/userMenu.h
@@ -0,0 +1,17 @@
+#ifndef adminMenu
+
+#define adminMenu
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <conio.h>
+#include <fstream>
+#include "cls.h"
+#include "pause.h"
+
+
+void UserMenu();
+bool User();
+#endif
