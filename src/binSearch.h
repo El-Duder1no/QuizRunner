72b74098964dea1fdd5b407e0ef4f510880c5e15
tryblt <iren09980@icloud.com>
2020-06-02 19:39:23 +0700

    Add BinSearch for Results struct

--- src/binSearch.h
+++ src/binSearch.h
@@ -2,9 +2,11 @@
 #define SEARCH_H
 
 #include "accountStruct.h"
+#include "resultStruct.h"
 #include <string>
 #include <vector>
+#include <cmath>
 
 int BinSearch(std::vector<Account>& a, int* ind, std::string key);
-
+int BinSearch(std::vector<Results>& a, int* ind, std::string key);
 #endif
