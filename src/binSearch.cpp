9f6818b930337d3273eb3929908413451f1ca8fc
tryblt <iren09980@icloud.com>
2020-06-02 20:40:28 +0700

    Remove BinSearch for Results struct

--- src/binSearch.cpp
+++ src/binSearch.cpp
@@ -17,21 +17,3 @@ int BinSearch(std::vector<Account>& a, int* ind, std::string key)
 		return -1;
 	}
 }
-
-int BinSearch(std::vector<Results>& a, int* ind, std::string key)
-{
-	int l = 0, r = a.size() - 1, m;
-	while (l < r) {
-		m = floor((l + r) / 2);
-		if (a[ind[m]].username < key)
-			l = m + 1;
-		else
-			r = m;
-	}
-	if (a[ind[r]].username == key) {
-		return r;
-	}
-	else {
-		return -1;
-	}
-}
