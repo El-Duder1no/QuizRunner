4f35da4f3d753eecc95b93401f3a742b2413a1f3
tryblt <iren09980@icloud.com>
2020-06-02 16:36:41 +0700

    Add funcions to userMenu.cpp

---
+++ src/userMenu.cpp
@@ -0,0 +1,39 @@
+#include "userMenu.h"
+void UserMenu()
+{
+    std::cout << "Выберите пункт меню:" << std::endl;
+    std::cout << "1 - Просмотр результатов" << std::endl;
+    std::cout << "2 - Начать тест" << std::endl;
+    std::cout << "ESC - Выход" << std::endl;
+    std::cout << ">> ";
+}
+
+bool User()
+{
+    bool userMenuState = true;
+    int chooseKey;
+        while (userMenuState) {
+            UserMenu();
+            switch (chooseKey = _getch()) {
+            case '1': {
+                CLS();
+                break;
+            }
+            case '2': {
+                CLS();
+                break;
+            }
+            case 27: {
+                userMenuState = false;
+                break;
+            }
+            default: {
+                std::cout << "Ошибка ввода" << std::endl;
+                PAUSE();
+                CLS();
+                break;
+            }
+            }
+        }
+        return true;
+}
