d3aa74b73419e2b70cc48098efe2ac92f617670d
Andrey <udovchenkoae@gmail.com>
2020-06-03 00:16:11 +0700

    Remove data input from login and registration functions

--- src/signInOut.cpp
+++ src/signInOut.cpp
@@ -1,32 +1,45 @@
-#include "signInOut.h"
+﻿#include "signInOut.h"
 
 void registrationMenu()
 {
     std::cout << "Выберите пункт меню:\n";
     std::cout << "1 - Вход в учетную запись\n";
-    std::cout << "2 - Регистраци¤\n";
+    std::cout << "2 - Регистрация\n";
     std::cout << "ESC - выход\n";
     std::cout << ">> ";
 }
+void registrationInfo()
+{
+    CLS();
 
-bool isLoginCorrect(std::string username)
+    std::cout << "Длина имени пользователя должна\n"
+              << "составлять от 3 до 20 символов.\n"
+              << "Также, оно может состоять \n"
+              << "из букв латинского алфавита, цифр или знаков\n\n";
+
+    std::cout << "Длина пароля от 8 до 25 символов.\n"
+              << "Пароль должен состоять из букв\n"
+              << "латинского алфавита и должен содержать\n"
+              << "как минимум одну заглавную букву и цифру.\n\n";
+}
+
+bool isLoginCorrect(const std::string username)
 {
     if (username.size() < 3 || username.size() > 20) {
         return false;
     }
 
-    int loginSize = username.length();
-    char* buff = new char[loginSize];
-    strcpy_s(buff, loginSize + 1, username.c_str());
+    char* buff = new char[username.length()];
+    strcpy_s(buff, username.length() + 1, username.c_str());
 
-    for (int i = 0; i < loginSize; i++) {
+    for (int i = 0; i < username.length(); i++) {
         if (isgraph(buff[i]) == false) {
             return false;
         }
     }
     return true;
 }
-bool isPassCorrect(std::string password)
+bool isPassCorrect(const std::string password)
 {
     if (password.size() < 8 || password.size() > 25) {
         return false;
@@ -83,65 +96,53 @@ void accountsParse(std::vector<Account>& a, const std::string accountPath)
     file.close();
 }
 
-bool enterAccount(std::vector<Account>& a, int* ind, std::string& User)
+bool enterAccount(
+        std::string& User,
+        std::string password,
+        std::string username,
+        const std::string accountPath)
 {
-    std::string username, password;
+    std::vector<Account> accounts;
+    accountsParse(accounts, accountPath);
 
-    CLS();
-
-    std::cout << "Введите логин:" << std::endl << ">>";
-    std::getline(std::cin, username);
-    std::cout << "Введите пароль:" << std::endl << ">>";
-    std::getline(std::cin, password);
+    int* index = new int[accounts.size()];
+    for (int i = 0; i < accounts.size(); i++) {
+        index[i] = i;
+    }
+    SelectSort(accounts, index);
 
     for (int i = 0; i < password.size(); i++) {
         password[i] += CAESAR_SHIFT;
     }
 
-    int temp = BinSearch(a, ind, username);
+    int temp = BinSearch(accounts, index, username);
 
-    if (temp != -1 && a[ind[temp]].password == password) {
+    if (temp != -1 && accounts[index[temp]].password == password) {
         User = username;
+        delete[] index;
         return true;
     } else {
+        delete[] index;
         return false;
     }
 }
 
 int registration(
-        std::vector<Account>& a,
-        std::string& username,
-        std::string& password,
+        const std::string username,
+        const std::string password,
         const std::string accountPath)
 {
-    CLS();
+    std::vector<Account> accounts;
+    accountsParse(accounts, accountPath);
 
-    std::cout << "Длина имени пользовател¤ должна\n"
-              << "составлять от 3 до 20 символов.\n"
-              << "Также, оно может состоять \n"
-              << "из букв латинского алфавита, цифр или знаков\n\n";
-
-    std::cout << "Длина пароля от 8 до 25 символов.\n"
-              << "Пароль должен состоять из букв\n"
-              << "латинского алфавита и должен содержать\n"
-              << "как минимум одну заглавную букву и цифру.\n\n";
-
-    std::cout << "Введите логин:\n>>";
-    std::getline(std::cin, username);
-    std::cout << "Введите пароль:\n>>";
-    std::getline(std::cin, password);
-
-    //---------
-    accountsParse(a, accountPath);
-    int* ind = new int[a.size()];
-    for (int i = 0; i < a.size(); i++) {
-        ind[i] = i;
+    int* index = new int[accounts.size()];
+    for (int i = 0; i < accounts.size(); i++) {
+        index[i] = i;
     }
-    SelectSort(a, ind);
+    SelectSort(accounts, index);
+    int temp = BinSearch(accounts, index, username);
 
-    int temp = BinSearch(a, ind, username);
-
-    delete[] ind;
+    delete[] index;
 
     if (temp != -1) {
         return 0;
@@ -163,16 +164,16 @@ void signInOut(const std::string accountPath, std::string& User)
         switch (chooseKey = _getch()) {
         // ВХОД
         case '1': {
-            std::vector<Account> accounts;
-            accountsParse(accounts, accountPath);
+            std::string username, password;
 
-            int* index = new int[accounts.size()];
-            for (int i = 0; i < accounts.size(); i++) {
-                index[i] = i;
-            }
-            SelectSort(accounts, index);
+            CLS();
 
-            bool temp = enterAccount(accounts, index, User);
+            std::cout << "Введите логин:\n>>";
+            std::getline(std::cin, username);
+            std::cout << "Введите пароль:\n>>";
+            std::getline(std::cin, password);
+
+            bool temp = enterAccount(User, password, username, accountPath);
 
             switch (temp) {
             case false: {
@@ -189,15 +190,20 @@ void signInOut(const std::string accountPath, std::string& User)
                 break;
             }
             }
-            delete[] index;
             break;
         }
         // РЕГИСТРАЦИЯ
         case '2': {
             std::string username, password;
-            std::vector<Account> accounts;
 
-            int temp = registration(accounts, username, password, accountPath);
+            registrationInfo();
+
+            std::cout << "Введите логин:\n>>";
+            std::getline(std::cin, username);
+            std::cout << "Введите пароль:\n>>";
+            std::getline(std::cin, password);
+
+            int temp = registration(username, password, accountPath);
             switch (temp) {
             case 0: {
                 CLS();
@@ -207,14 +213,16 @@ void signInOut(const std::string accountPath, std::string& User)
                 break;
             }
             case 1: {
-                std::cout << "Регистраци¤ прошла успешно\n";
+                std::cout << "Регистрация прошла успешно\n";
                 User = username;
 
                 std::ofstream file(
                         accountPath, std::ios_base::app | std::ios_base::ate);
 
-                if (!file.is_open())
+                if (!file.is_open()) {
                     std::cerr << "\nФайл не открыт!\n";
+                    break;
+                }
 
                 for (int i = 0; i < password.size(); i++) {
                     password[i] += CAESAR_SHIFT;
