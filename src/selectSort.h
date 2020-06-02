d7e246bd531734334a88b9f3c0e1e9f73b7bd614
tryblt <iren09980@icloud.com>
2020-06-02 19:39:55 +0700

    Add SelectSort for Results struct

--- src/selectSort.h
+++ src/selectSort.h
@@ -2,9 +2,10 @@
 #define SELECT_SORT_H
 
 #include "accountStruct.h"
+#include "resultStruct.h"
 #include <utility>
 #include <vector>
 
 void SelectSort(std::vector<Account>& a, int* ind);
-
+void SelectSort(std::vector<Results>& a, int* ind);
 #endif
