e132007d29f8dd50b67aa95c25c116aee3f81374
Andrey <udovchenkoae@gmail.com>
2020-06-03 19:57:33 +0700

    Write parser for Results.txt

---
+++ src/resultsParse.h
@@ -0,0 +1,11 @@
+﻿#ifndef RESULT_PARSE_H
+#define RESULT_PARSE_H
+
+#include "resultStruct.h"
+#include "parseString.h"
+#include <vector>
+#include <fstream>
+
+void resultsParse(std::vector<Results>& a, std::ifstream& results);
+
+#endif
