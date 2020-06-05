3c6964c50ae1f6e6b2b592257a66057fd199e393
Andrey <udovchenkoae@gmail.com>
2020-06-06 02:10:52 +0700

    Write test for testPassing

--- test/testPassing-TEST.cpp
+++ test/testPassing-TEST.cpp
@@ -1,6 +1,6 @@
 #include <fstream>
-#include <gtest/gtest.h>
 #include <vector>
+#include <gtest/gtest.h>
 
 #include "Questions.h"
 #include "currentUserStruct.h"
@@ -9,60 +9,66 @@
 #include "testPassing.h"
 
 #ifdef _WIN32
-#define RESULTS_PATH "..\\res\\test_files\\ResultsTestPassing.txt"
+#define RESULTS_PATH "..\\res\\test_files\\ResultsWrite.txt"
 #define WRONG_RESULTS_PATH "..\\res\\test_files"
+#define QUIZ_PATH "..\\res\\test_files\\Questions.txt"
 #else
-#define RESULTS_PATH "res/test_files/ResultsTestPassing.txt"
+#define RESULTS_PATH "res/test_files/ResultsWrite.txt"
 #define WRONG_RESULTS_PATH "res/test_files"
+#define QUIZ_PATH "res/test_files/Questions.txt"
 #endif
 
 TEST(checkAnswerTest, allOptions)
 {
-    std::vector<Questions> test;
-    std::vector<std::string> answers;
+	std::vector<Questions> test;
+	std::vector<std::string> answers;
 
-    answers.push_back("0");
-    answers.push_back("0");
-    answers.push_back("0");
-    EXPECT_FLOAT_EQ(checkAnswers(test, answers), 100);
+	testFilling("", QUIZ_PATH, test);
 
-    answers.clear();
-    answers.push_back("0");
-    answers.push_back("0");
-    answers.push_back("0");
-    EXPECT_FLOAT_EQ(checkAnswers(test, answers), 0);
+	answers.push_back("0.5");
+	answers.push_back("2 3 4 5");
+	answers.push_back("10");
+	EXPECT_FLOAT_EQ(checkAnswers(test, answers), 100);
 
-    answers.clear();
-    answers.push_back("0");
-    answers.push_back("0");
-    answers.push_back("0");
-    EXPECT_FLOAT_EQ(checkAnswers(test, answers), 33);
+	answers.clear();
+	answers.push_back("0");
+	answers.push_back("0");
+	answers.push_back("0");
+	EXPECT_FLOAT_EQ(checkAnswers(test, answers), 0);
 
-    answers.clear();
-    answers.push_back("0");
-    answers.push_back("0");
-    answers.push_back("0");
-    EXPECT_FLOAT_EQ(checkAnswers(test, answers), 66);
+	answers.clear();
+	answers.push_back("0.5");
+	answers.push_back("0");
+	answers.push_back("0");
+	EXPECT_FLOAT_EQ(checkAnswers(test, answers), 33);
+
+	answers.clear();
+	answers.push_back("0.5");
+	answers.push_back("2 3 4 5");
+	answers.push_back("0");
+	EXPECT_FLOAT_EQ(checkAnswers(test, answers), 66);
 }
 
 TEST(writeResultsTest, correct)
 {
-    std::string result = "100", time = "67";
-    currentUser currUser;
-    currUser.username = "OlegPetrovich";
-    currUser.accountType = 0;
+	std::string result = "100", time = "67";
+	currentUser currUser;
+	currUser.username = "Oleg";
+	currUser.accountType = 0;
 
-    EXPECT_TRUE(writeResults(RESULTS_PATH, currUser, result, time));
+	EXPECT_TRUE(writeResults(RESULTS_PATH, currUser, result, time));
 
-    std::fstream file(RESULTS_PATH, std::ios_base::trunc);
+	std::ofstream file(RESULTS_PATH, std::ios_base::trunc);
 }
 
 TEST(writeResultsTest, incorrect)
 {
-    std::string result = "100", time = "67";
-    currentUser currUser;
-    currUser.username = "OlegPetrovich";
-    currUser.accountType = 0;
+	std::string result = "100", time = "67";
+	currentUser currUser;
+	currUser.username = "OlegPetrovich";
+	currUser.accountType = 0;
 
-    EXPECT_FALSE(writeResults(WRONG_RESULTS_PATH, currUser, result, time));
+	EXPECT_FALSE(writeResults(WRONG_RESULTS_PATH, currUser, result, time));
+
+	std::ofstream file(RESULTS_PATH, std::ios_base::trunc);
 }
