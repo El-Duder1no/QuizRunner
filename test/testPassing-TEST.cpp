8fcbe4aeb9e41a3331c340286ee1d5502f2c079e
Andrey <udovchenkoae@gmail.com>
2020-06-06 02:13:15 +0700

    Code style fix

--- test/testPassing-TEST.cpp
+++ test/testPassing-TEST.cpp
@@ -1,6 +1,6 @@
 #include <fstream>
-#include <vector>
 #include <gtest/gtest.h>
+#include <vector>
 
 #include "Questions.h"
 #include "currentUserStruct.h"
@@ -20,55 +20,55 @@
 
 TEST(checkAnswerTest, allOptions)
 {
-	std::vector<Questions> test;
-	std::vector<std::string> answers;
+    std::vector<Questions> test;
+    std::vector<std::string> answers;
 
-	testFilling("", QUIZ_PATH, test);
+    testFilling("", QUIZ_PATH, test);
 
-	answers.push_back("0.5");
-	answers.push_back("2 3 4 5");
-	answers.push_back("10");
-	EXPECT_FLOAT_EQ(checkAnswers(test, answers), 100);
+    answers.push_back("0.5");
+    answers.push_back("2 3 4 5");
+    answers.push_back("10");
+    EXPECT_FLOAT_EQ(checkAnswers(test, answers), 100);
 
-	answers.clear();
-	answers.push_back("0");
-	answers.push_back("0");
-	answers.push_back("0");
-	EXPECT_FLOAT_EQ(checkAnswers(test, answers), 0);
+    answers.clear();
+    answers.push_back("0");
+    answers.push_back("0");
+    answers.push_back("0");
+    EXPECT_FLOAT_EQ(checkAnswers(test, answers), 0);
 
-	answers.clear();
-	answers.push_back("0.5");
-	answers.push_back("0");
-	answers.push_back("0");
-	EXPECT_FLOAT_EQ(checkAnswers(test, answers), 33);
+    answers.clear();
+    answers.push_back("0.5");
+    answers.push_back("0");
+    answers.push_back("0");
+    EXPECT_FLOAT_EQ(checkAnswers(test, answers), 33);
 
-	answers.clear();
-	answers.push_back("0.5");
-	answers.push_back("2 3 4 5");
-	answers.push_back("0");
-	EXPECT_FLOAT_EQ(checkAnswers(test, answers), 66);
+    answers.clear();
+    answers.push_back("0.5");
+    answers.push_back("2 3 4 5");
+    answers.push_back("0");
+    EXPECT_FLOAT_EQ(checkAnswers(test, answers), 66);
 }
 
 TEST(writeResultsTest, correct)
 {
-	std::string result = "100", time = "67";
-	currentUser currUser;
-	currUser.username = "Oleg";
-	currUser.accountType = 0;
+    std::string result = "100", time = "67";
+    currentUser currUser;
+    currUser.username = "Oleg";
+    currUser.accountType = 0;
 
-	EXPECT_TRUE(writeResults(RESULTS_PATH, currUser, result, time));
+    EXPECT_TRUE(writeResults(RESULTS_PATH, currUser, result, time));
 
-	std::ofstream file(RESULTS_PATH, std::ios_base::trunc);
+    std::ofstream file(RESULTS_PATH, std::ios_base::trunc);
 }
 
 TEST(writeResultsTest, incorrect)
 {
-	std::string result = "100", time = "67";
-	currentUser currUser;
-	currUser.username = "OlegPetrovich";
-	currUser.accountType = 0;
+    std::string result = "100", time = "67";
+    currentUser currUser;
+    currUser.username = "OlegPetrovich";
+    currUser.accountType = 0;
 
-	EXPECT_FALSE(writeResults(WRONG_RESULTS_PATH, currUser, result, time));
+    EXPECT_FALSE(writeResults(WRONG_RESULTS_PATH, currUser, result, time));
 
-	std::ofstream file(RESULTS_PATH, std::ios_base::trunc);
+    std::ofstream file(RESULTS_PATH, std::ios_base::trunc);
 }
