d8e42dda66c008f2c7226a01cd046c8265a12330
tryblt <iren09980@icloud.com>
2020-06-05 17:58:39 +0700

    Add tests for checkPath functions

---
+++ test/checkPath-TEST.cpp
@@ -0,0 +1,44 @@
+#include <fstream>
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+
+#include "Questions.h"
+#include "resultStruct.h"
+#include "testFunctions.h"
+
+#ifdef _WIN32
+#define RESULTS_PATH "..\\res\\test_files\\Results.txt"
+#define QUESTIONS_PATH "..\\res\\test_files\\Questions.txt"
+#define NO_FILE_PATH "\\res\\test_files\\"
+#else
+#define QUESTIONS_PATH "../res/test_files/Questions.txt"	
+#define RESULTS_PATH "/res/test_files/Results.txt"
+#define NO_FILE_PATH "/res/test_files/"
+#endif
+
+std::vector<Results> userResult;
+std::vector<Questions> question;
+
+TEST(CheckPath, InCorrectPATH)
+{
+    std::ifstream ResultsPath(NO_FILE_PATH);
+    EXPECT_FALSE(CheckPath(ResultsPath, userResult));
+
+    std::ifstream QuestionsPath(NO_FILE_PATH);
+    EXPECT_FALSE(CheckPath(QuestionsPath, question));
+    question.clear();
+    userResult.clear();
+}
+
+TEST(CheckPath, CorrectPATH)
+{
+    std::ifstream File(RESULTS_PATH);
+    EXPECT_TRUE(CheckPath(File, userResult));
+
+    std::ifstream QuestionsFile(QUESTIONS_PATH);
+    EXPECT_TRUE(CheckPath(QuestionsFile, question));
+
+    question.clear();
+    userResult.clear();
+}
