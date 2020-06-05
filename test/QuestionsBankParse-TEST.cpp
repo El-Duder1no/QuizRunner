a1f703218549dfdb8fca33a4180ac5fa03c6681b
tryblt <iren09980@icloud.com>
2020-06-05 20:03:49 +0700

    Add tests for parseForTXT.cpp functions

---
+++ test/QuestionsBankParse-TEST.cpp
@@ -0,0 +1,34 @@
+#include "resultStruct.h"
+#include "resultsParse.h"
+#include "parseForTXT.h"
+#include "Questuions.h"
+#include <fstream>
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+
+#ifdef _WIN32
+#define QUESTIONS_PATH "..\\res\\test_files\\Questions.txt"
+#define TEST_PATH "..\\res\\test_files\\Test.txt"
+#else
+#define QUESTIONS_PATH "res/test_files/Questions.txt"
+#define TEST_PATH "res/test_files/Test.txt"
+#endif
+
+TEST(QuestionsParse, CORRECT_PARSE)
+{
+    std::ifstream QuestionsFile(QUESTIONS_PATH);
+    std::vector<Questions> actual;
+    QuestionsFile.seekg(std::ios_base::beg);
+    std::string type;
+    getline(QuestionsFile, type, '\n');
+    EXPECT_TRUE(ParseThirdQuestions(QuestionsFile, actual));
+ 
+	getline(QuestionsFile, type, '\n');
+    EXPECT_TRUE(ParseSecondQuestions(QuestionsFile, actual));
+    
+	getline(QuestionsFile, type, '\n');
+    EXPECT_TRUE(ParseThirdQuestions(QuestionsFile, actual));
+    
+    question.clear();
+}
