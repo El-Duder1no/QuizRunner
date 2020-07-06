da5d8e4987ca42aefb331a01a3c609270ad16c3c
tryblt <iren09980@icloud.com>
2020-07-07 00:03:03 +0700

    Fix code style

--- test/QuestionsBankParse-TEST.cpp
+++ test/QuestionsBankParse-TEST.cpp
@@ -85,8 +85,8 @@ TEST(TestParse, CORRECT_PARSE)
     TestFile.seekg(std::ios_base::beg);
     std::string type;
     getline(TestFile, type, '\n');
-    ParseTest(TestFile, actual, type)
-	int questionSize = actual.size();
+    ParseTest(TestFile, actual, type);
+    int questionSize = actual.size();
 
     for (int i = 0; i < questionSize; ++i) {
         ASSERT_STREQ(actual[i].type.c_str(), expected[i].type.c_str());
