9ed558e1c6f768709a6b552b933cdbbeb360f07a
El-Duder1no <udovchenkoae@gmail.com>
2020-07-11 14:59:43 +0700

    Delete unnecessary libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,18 @@
-#include "Questions.h"
 #include "adminMenu.h"
-#include "binSearch.h"
 #include "currentUserStruct.h"
-#include "parseString.h"
-#include "resultStruct.h"
-#include "screenFunctions.h"
-#include "selectSort.h"
 #include "signInOut.h"
-#include "testPassing.h"
 #include "userMenu.h"
 
-#include <fstream>
 #include <iostream>
-#include <sstream>
-#include <stdlib.h>
 #include <string>
-#include <vector>
 
 using namespace std;
 
-#ifdef _WIN32
-#define ACCOUNTS_PATH "..\\res\\Accounts.txt"
-#define TEST_PATH "..\\res\\Test.txt"
-#define QUESTIONS_PATH "..\\res\\Questions.txt"
-#define NOT_IN_TEST_PATH "..\\res\\NotInTheTest.txt"
-#define RESULTS_PATH "..\\res\\Results.txt"
-#else
 #define ACCOUNTS_PATH "res/Accounts.txt"
 #define TEST_PATH "res/Test.txt"
 #define QUESTIONS_PATH "res/Questions.txt"
 #define NOT_IN_TEST_PATH "res/NotInTheTest.txt"
 #define RESULTS_PATH "res/Results.txt"
-#endif
 
 int main()
 {
