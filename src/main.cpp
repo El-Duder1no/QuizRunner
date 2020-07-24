d5410195b5b663f54388a1ccc1f30d6a764dcdf8
El-Duder1no <udovchenkoae@gmail.com>
2020-07-24 14:43:20 +0700

    Update file paths

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,36 @@
 #include <iostream>
 #include <string>
 
+#include <fstream>
+
 using namespace std;
 
-#define ACCOUNTS_PATH "res/Accounts.txt"
-#define TEST_PATH "res/Test.txt"
-#define QUESTIONS_PATH "res/Questions.txt"
-#define NOT_IN_TEST_PATH "res/NotInTheTest.txt"
-#define RESULTS_PATH "res/Results.txt"
+#define ACCOUNTS_PATH "../res/Accounts.txt"
+#define TEST_PATH "../res/Accounts.txt"
+#define QUESTIONS_PATH "../res/Accounts.txt"
+#define NOT_IN_TEST_PATH "../res/NotInTheTest.txt"
+#define RESULTS_PATH "../res/Results.txt"
 
 int main()
 {
     setlocale(LC_ALL, "RUS");
+
+    ifstream acc(ACCOUNTS_PATH);
+    ifstream test(TEST_PATH);
+    ifstream quest(QUESTIONS_PATH);
+    ifstream ntest(TEST_PATH);
+    ifstream res(RESULTS_PATH);
+
+    if (!acc.is_open())
+        cout << "acc jopa!\n";
+    if (!test.is_open())
+        cout << "test jopa!\n";
+    if (!quest.is_open())
+        cout << "quest jopa!\n";
+    if (!ntest.is_open())
+        cout << "ntest jopa!\n";
+    if (!res.is_open())
+        cout << "res jopa!\n";
 
     currentUser currUser;
 
