41e632ff79b6b58f6690bdd9705aed14a34d069d
tryblt <iren09980@icloud.com>
2020-08-01 20:29:14 +0700

    Update functions

--- src/testFunctions.cpp
+++ src/testFunctions.cpp
@@ -1,65 +1,66 @@
 #include "testFunctions.h"
 
-bool CheckPath(std::ifstream& questions, std::vector<Questions>& QuestionsBank)
+bool CheckPath(std::ifstream& questions)
 {
     if (!questions) {
         return false;
-    } else {
-        questions.seekg(std::ios_base::beg);
-        while (!questions.eof()) {
-            std::string type;
-            getline(questions, type, '\n');
-            switch (atoi(type.c_str())) {
-            case 1: {
-                if (!ParseFirstQuestions(questions, QuestionsBank)) {
-                    return false;
-                }
-                break;
-            }
-            case 2: {
-                if (!ParseSecondQuestions(questions, QuestionsBank)) {
-                    return false;
-                }
-                break;
-            }
-            case 3: {
-                if (!ParseThirdQuestions(questions, QuestionsBank)) {
-                    return false;
-                }
-                break;
-            }
-            }
-        }
-        return true;
     }
+    return true;
 }
 
-bool CheckPath(std::fstream& questions, std::vector<Questions>& QuestionForTest)
+bool CheckPath(std::fstream& questions)
 {
     if (!questions) {
         return false;
-    } else {
-        questions.seekg(std::ios_base::beg);
-        while (!questions.eof()) {
-            std::string type;
-            getline(questions, type, '\n');
-            if (!(type == "")) {
-                ParseTest(questions, QuestionForTest, type);
-            }
+    }
+    return true;
+}
+
+void parseRes(
+        std::ifstream& results,
+        std::vector<Results>& userResult) // no questions ok?
+{
+    resultsParse(results, userResult);
+}
+
+void parseTest(std::fstream& questions, std::vector<Questions>& QuestionForTest)
+{
+    questions.seekg(std::ios_base::beg);
+    while (!questions.eof()) {
+        std::string type;
+        getline(questions, type, '\n');
+        if (!(type == "")) {
+            ParseTest(questions, QuestionForTest, type);
         }
-        return true;
     }
 }
 
-bool CheckPath(std::ifstream& results, std::vector<Results>& userResult)
+bool parseQuestions(
+        std::ifstream& questions, std::vector<Questions>& QuestionsBank)
 {
-    if (!results) {
-        return false;
-    } else {
-        results.seekg(std::ios_base::beg);
-        while (!results.eof()) {
-            resultsParse(results, userResult);
+    questions.seekg(std::ios_base::beg);
+    while (!questions.eof()) {
+        std::string type;
+        getline(questions, type, '\n');
+        switch (atoi(type.c_str())) {
+        case 1: {
+            if (!ParseFirstQuestions(questions, QuestionsBank)) {
+                return false;
+            }
+            break;
         }
-        return true;
+        case 2: {
+            if (!ParseSecondQuestions(questions, QuestionsBank)) {
+                return false;
+            }
+            break;
+        }
+        case 3: {
+            if (!ParseThirdQuestions(questions, QuestionsBank)) {
+                return false;
+            }
+            break;
+        }
+        }
     }
 }
